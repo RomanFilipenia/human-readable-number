f10960f0af8e462dd585294320b4136fb3690400
RomanFilipenia <biologg@mail.ru>
2020-10-12 19:07:24 +0300

    new

--- src/index.js
+++ src/index.js
@@ -25,8 +25,8 @@ module.exports = function toReadable (s) {
                 sk=1;
             }
         } else if (n[i]!=0) { // 0235
-            if(i==x-1){str += dg[n[i]];}
-             else {str += dg[n[i]]+' ';}
+           // if(i==x-1){str += dg[n[i]];}
+             str += dg[n[i]]+' ';
             if ((x-i)%3==0) str += 'hundred ';
             sk=1;
         }
